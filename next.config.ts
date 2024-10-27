b10231eab08f7a93110e0b674539509e180ce1e0
Kal Parvanov <parvanovkaloyan@gmail.com>
2024-10-27 00:25:39 -0600

    Adding longer timeout limit.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      timeout: 60000, // 60 seconds
+      bodySizeLimit: '2mb'
+    }
+  }
 };
 
 export default nextConfig;
